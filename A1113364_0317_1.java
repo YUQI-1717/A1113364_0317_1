d4cc1864ee21f8dc0f6b622763ef03fd87ec9854
YUQI-1717 <125873508+YUQI-1717@users.noreply.github.com>
2023-03-23 22:16:02 +0800

    Update A1113364_0317_1.java

--- A1113364_0317_1.java
+++ A1113364_0317_1.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class A1113364_0317_1 {
-    public static void main(String[] argv) {
+    public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
         System.out.print("請輸入n的值:");
